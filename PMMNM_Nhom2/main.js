fca95a48c25448903b2ae25849aed7de6f2ec366
DoTienKhai <dotienkhai06@gmail.con>
2021-04-04 17:46:11 +0700

    Giao dien xong, chua responsive

--- PMMNM_Nhom2/main.js
+++ PMMNM_Nhom2/main.js
@@ -7,3 +7,18 @@ window.addEventListener('scroll', function(){
     let header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+function Dong_ho() {
+    var gio = document.getElementById("gio");
+    var phut = document.getElementById("phut");
+    var giay = document.getElementById("giay");
+    var Gio_hien_tai = new Date().getHours();
+    var Phut_hien_tai = new Date().getMinutes();
+    var Giay_hien_tai = new Date().getSeconds();
+    gio.innerHTML = Gio_hien_tai;
+    phut.innerHTML = Phut_hien_tai;
+    giay.innerHTML = Giay_hien_tai;
+}
+var Dem_gio = setInterval(Dong_ho, 1000);
+
